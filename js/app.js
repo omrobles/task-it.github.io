5cc4dfc1037c974590ea82869ddc71b8a22688c5
omrobles <omrobles.dwfc4@ucamp.utel.edu.mx>
2022-01-31 19:19:38 -0600

    Se crea formulario y LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-let task = document.getElementById("task")
-let create = document.getElementById("button")
 
-create.addEventListener("click", function(){
-    console.log(task)
+alert("estoy coneectado")
+let taskTitle = document.getElementById("task");
+let taskDescription = document.getElementById("description");
+let taskResponsible = document.getElementById("responsible");
+let createButton = document.getElementById("button");
+let tasks=[];
+
+button.addEventListener("click",function(){
+    tasks.push({
+        task: taskTitle.value,
+        description: taskDescription.value,
+        responsible: taskResponsible.value
+    })
+    console.log(tasks)
+    localStorage.setItem('tasksList', JSON.stringify(tasks));
 });
