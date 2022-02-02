78457c6b5bec2d1706b81420f1cbd8c66cc8f5a0
omrobles <omrobles.dwfc4@ucamp.utel.edu.mx>
2022-02-01 19:58:24 -0600

    Se crea funcion para desplegar datos (READ)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-
-alert("estoy coneectado")
 let taskTitle = document.getElementById("task");
 let taskDescription = document.getElementById("description");
 let taskResponsible = document.getElementById("responsible");
 let createButton = document.getElementById("button");
+let list = document.getElementById("listaTareas");
 let tasks=[];
 
-button.addEventListener("click",function(){
+createButton.addEventListener("click",function(){
     tasks.push({
         task: taskTitle.value,
         description: taskDescription.value,
         responsible: taskResponsible.value
     })
-    console.log(tasks)
-    localStorage.setItem('tasksList', JSON.stringify(tasks));
+    storeData();
+    readData();
 });
+
+function storeData(){
+    localStorage.setItem('tasksList', JSON.stringify(tasks));
+}
+
+function readData (){
+    tasks = JSON.parse(localStorage.getItem('tasksList'));
+    list.innerHTML = "<ul></ul>"
+    tasks.forEach(task => {
+        list.innerHTML += '<li><div >' + task['task'] + '<br>' + task['description'] + '</div><div>' + task['responsible'] + '</div><div class="icon"><ion-icon name="create-outline"></ion-icon><ion-icon name="trash-outline"></ion-icon></div></li>'; 
+    });
+}
