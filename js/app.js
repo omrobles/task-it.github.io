425474def81a87e7669b160015ab15823b4d35ee
omrobles <omrobles.dwfc4@ucamp.utel.edu.mx>
2022-02-07 00:24:26 -0600

    Se crean mas estilos y correciones de bugs

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ let taskTitle = document.getElementById("task"),
     updateButton = document.getElementById("updateButton"),
     h2Message = document.querySelector('h2'),
     errorMessage = document.getElementById("errorMessage"),
+    form = document.getElementById("form"),
+    prevScrollpos = window.pageYOffset,
     list = document.getElementById("listaTareas");
 let tasks=[];
 
@@ -42,12 +44,25 @@ updateButton.addEventListener('click', function(){
     taskTitle.select();
 });
 
+// funcion ocultar forma
+window.addEventListener('scroll',function(){
+    let currentScrollPos = window.pageYOffset;
+   	if (prevScrollpos > currentScrollPos) {
+        form.setAttribute('style', 'top: 70px;');
+  	 	//  form.setAttribute('style', 'top: ' + currentScrollPos + 'px;');
+  		} else {
+  	 	 form.setAttribute('style', 'top: -300px;');
+  		}
+  		prevScrollpos = currentScrollPos;
+});
+
 // Funcion para empujar datos al array tasks
 function taskPush(){
     tasks.push({
         task: taskTitle.value,
         description: taskDescription.value,
-        responsible: taskResponsible.value
+        responsible: taskResponsible.value,
+        check: false
     });
 };
 
@@ -83,9 +98,12 @@ function insertLi(task) {
     // Crear los elementos del li
     let liNew = document.createElement('li'),
         divTaskTitle = document.createElement('div'),
+        divIcons = document.createElement('div'),
         divTaskDescription = document.createElement('div'),
-        divResponsible = document.createElement('div'), 
+        divResponsible = document.createElement('div'),
+        checkIcon = document.createElement('ion-icon'), 
         editIcon = document.createElement('ion-icon'),
+        copyIcon = document.createElement('ion-icon'),
         removeIcon = document.createElement('ion-icon');
         
         // Agrega el contenido de los valores del localStorage
@@ -93,8 +111,18 @@ function insertLi(task) {
         divTaskDescription.innerText = task['description'];
         divResponsible.innerText = task['responsible'];
 
+        // Funcionamiento icono check
+        checkIcon.setAttribute('name', 'checkmark-outline');
+        checkIcon.setAttribute('class', 'noCheck');
+        checkIcon.addEventListener('click', function (){
+            liIndex = tasks.indexOf(task);
+            checkIcon.setAttribute('class', 'checked');
+            console.log('estas presionanso el check')
+        });
+
         // Funcionamiento icono edit
         editIcon.setAttribute('name', 'create-outline');
+        editIcon.setAttribute('class', 'editIcon');
         editIcon.addEventListener('click', function (){
             liIndex = tasks.indexOf(task);
             taskTitle.value = tasks[liIndex].task;
@@ -102,10 +130,23 @@ function insertLi(task) {
             taskResponsible.value = tasks[liIndex].responsible;
             createButton.setAttribute('class', 'hide');
             updateButton.setAttribute('class', 'show');
+            form.setAttribute('style', 'top: 70px;');
+        });
+
+        // Funcionamiento icono copy
+        copyIcon.setAttribute('name', 'copy-outline');
+        copyIcon.setAttribute('class', 'copyIcon');
+        copyIcon.addEventListener('click', function (){
+            liIndex = tasks.indexOf(task);
+            taskTitle.value = tasks[liIndex].task;
+            taskDescription.value = tasks[liIndex].description;
+            taskResponsible.value = tasks[liIndex].responsible;
+            form.setAttribute('style', 'top: 70px;');
         });
 
         // Funcionamiento icono eliminar
         removeIcon.setAttribute('name', 'trash-outline');
+        removeIcon.setAttribute('class', 'removeIcon');
         removeIcon.addEventListener('click', function (){
             liIndex = tasks.indexOf(task);
             tasks.splice(liIndex,1);
@@ -113,11 +154,15 @@ function insertLi(task) {
             readData();
         });
 
+        divIcons.appendChild(checkIcon);
+        divIcons.appendChild(editIcon);
+        divIcons.appendChild(copyIcon);
+        divIcons.appendChild(removeIcon);
+
         liNew.appendChild(divTaskTitle);
+        liNew.appendChild(divIcons);
         liNew.appendChild(divTaskDescription);
         liNew.appendChild(divResponsible);
-        liNew.appendChild(removeIcon);
-        liNew.appendChild(editIcon);
         return liNew;
 };
 
